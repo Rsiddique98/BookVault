985174ad27e2e1914c76940b1d9841376f4e98ac
Rsiddique98 <ridasiddique@yahoo.com>
2025-04-12 00:03:42 +0100

    Additional styling on review page

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 
+/* JS code to allow navbar colour change from transparent to opaque upon scrolling */
+
 window.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 10) {
